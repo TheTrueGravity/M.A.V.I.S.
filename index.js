645404e320af8476c6a315f1be7d01ba7aed1b2c
Charlie Armstrong <charliearmstrong180@gmail.com>
2020-12-08 16:14:09 +0000

    v1.0

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const util = require('./utils/util');
 
-var net = new util.network("test")
-
-net.test()
-//util.speak.run("test command")
+var net = new util.network("test", [
+    {"input": [0, 0, 1], "output": [0]},
+    {"input": [0, 1, 0], "output": [0]},
+    {"input": [1, 0, 1], "output": [1]},
+    {"input": [1, 1, 0], "output": [1]},
+])
--- utils/network.js
+++ utils/network.js
@@ -1,8 +1,22 @@
-const brain = require('brain.js')
+const brain = require('brain.js');
+const fs = require('fs')
 
-module.exports = function (name) {
+module.exports = function (name, training_data=null) {
     this.name = name
-    this.test = function() {
-        console.log(this.name)
+    if(training_data) {
+        train(training_data)
+    }
+    function train(training_data=null) {
+        var data = []
+        if (training_data) {
+            data = training_data
+        } else {
+            const d = require('./net-data/training-data.json')
+            if(d[this.name]) {
+                data = d[this.name]
+            } else {
+                console.error(new Error("No training data was provided!"))
+            }
+        }
     }
 }
