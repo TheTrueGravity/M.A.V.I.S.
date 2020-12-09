3da2bf2543c5e52210296b1c5de5d73ccbc61a5f
Charlie Armstrong <charliearmstrong180@gmail.com>
2020-12-09 14:57:53 +0000

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 const collections = require('./utils/collections');
 const util = require('./utils/util');
-
-var AIs = new util.collections()
-
-AIs.set("test", new util.network_num("test"))
-
-console.log(AIs.get("test").network.run([0, 1, 1])[0].toFixed(0))
