ce3de3822577372c6b37731d0b3666f5fffba808
Charlie Armstrong <charliearmstrong180@gmail.com>
2020-12-21 20:11:57 +0000

    v1.0

--- src/communication_server.js
+++ src/communication_server.js
@@ -1,15 +1,18 @@
 var mavis_utils = require('mavis-utils')
 
+var command_network = new mavis_utils.network_str('get_command');
+
 var server = new mavis_utils.mavis_local_clients.communication_server(6767, (packet) => {
     var packet_type = packet.packet
     if (packet_type.startsWith('test_')) return {packet: `${packet_type} recieved!`}
-    else if(packet_type == "generate_new_key") {
-        console.log("New key...")
-        var user_key = mavis_utils.mavis_local_clients.make_key(packet["data"]["friendly_name"])
+    else if (packet_type == 'get_command') { 
+        console.log(packet.data.speech); 
         return {
             packet: `${packet_type} recieved!`,
-            data: user_key
-        }
+            data: {
+                command: command_network.resolve(command_network.network.run(packet.data.speech))
+            }
+        };
     }
     else return {packet: `Unknown packet '${packet_type}'!`}
 })
